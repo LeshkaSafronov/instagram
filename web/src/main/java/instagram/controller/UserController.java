b5f2f06f90a29a7930b7929bdbd489690c4d92d1
Alexey Safronov <alexey.safronov@synesis.ru>
2018-05-27 20:22:16 +0300

    instagram: latest update

--- web/src/main/java/instagram/controller/UserController.java
+++ web/src/main/java/instagram/controller/UserController.java
@@ -3,6 +3,7 @@ package instagram.controller;
 import instagram.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +13,11 @@ public class UserController {
     @GetMapping("/register")
     public String register(UserDto userDto) {
         return "users/register";
+    }
+
+    @PostMapping("/register")
+    public void registerUser(UserDto userDto) {
+        System.out.println(userDto);
     }
 
     @GetMapping("/login")
